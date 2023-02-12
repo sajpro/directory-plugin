99941c05a472a22e5f5fd061b8f2666862a639aa
stalukder03 <stalukder03@gmail.com>
2023-02-12 16:09:35 +0600

    new listing class created to handle all listing in the same place

---
+++ includes/Admin/Listings.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Listings
+ *
+ * @package DirectoryPlugin
+ */
+
+namespace Sajib\DP\Admin;
+
+/**
+ * Listings handler class
+ */
+class Listings {
+
+	/**
+	 * Plugin page handler
+	 *
+	 * @return void
+	 */
+	public static function plugin_page() {
+		echo 'listing page';
+	}
+
+}
--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -25,16 +25,16 @@ class Menu {
 		$parent_slug = 'directory-listings';
 		$capability  = 'manage_options';
 
-		$hook = add_menu_page( __( 'Directory Plugin', 'directory-plugin' ), __( 'Directory Plugin', 'directory-plugin' ), $capability, $parent_slug, [ $this, 'plugin_page' ], 'dashicons-welcome-learn-more' );
-		add_submenu_page( $parent_slug, __( 'All Listings', 'directory-plugin' ), __( 'All Listings', 'directory-plugin' ), $capability, $parent_slug, [ $this, 'plugin_page' ] );
+		$hook = add_menu_page( __( 'Directory Plugin', 'directory-plugin' ), __( 'Directory Plugin', 'directory-plugin' ), $capability, $parent_slug, [ $this, 'all_listings_page' ], 'dashicons-welcome-learn-more' );
+		add_submenu_page( $parent_slug, __( 'All Listings', 'directory-plugin' ), __( 'All Listings', 'directory-plugin' ), $capability, $parent_slug, [ $this, 'all_listings_page' ] );
 		add_submenu_page( $parent_slug, __( 'Add Listing', 'directory-plugin' ), __( 'Add Listing', 'directory-plugin' ), $capability, 'directory-listings-create', [ $this, 'create_listings' ] );
 	}
 
 	/**
 	 * Plugin Page
 	 */
-	public function plugin_page() {
-		echo 'hello directory plugin page';
+	public function all_listings_page() {
+		Listings::plugin_page();
 	}
 
 	/**
