ab3d3c9ddb6de020b8721018d3dca2035663a8bf
stalukder03 <stalukder03@gmail.com>
2023-02-12 15:55:51 +0600

    cleanup

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -22,12 +22,12 @@ class Menu {
 	 * Admin Menu
 	 */
 	public function admin_menu() {
-		$parent_slug = 'directory-plugin';
+		$parent_slug = 'directory-listings';
 		$capability  = 'manage_options';
 
 		$hook = add_menu_page( __( 'Directory Plugin', 'directory-plugin' ), __( 'Directory Plugin', 'directory-plugin' ), $capability, $parent_slug, [ $this, 'plugin_page' ], 'dashicons-welcome-learn-more' );
 		add_submenu_page( $parent_slug, __( 'All Listings', 'directory-plugin' ), __( 'All Listings', 'directory-plugin' ), $capability, $parent_slug, [ $this, 'plugin_page' ] );
-		add_submenu_page( $parent_slug, __( 'Add Listing', 'directory-plugin' ), __( 'Add Listing', 'directory-plugin' ), $capability, 'directory-plugin-create', [ $this, 'create_listings' ] );
+		add_submenu_page( $parent_slug, __( 'Add Listing', 'directory-plugin' ), __( 'Add Listing', 'directory-plugin' ), $capability, 'directory-listings-create', [ $this, 'create_listings' ] );
 	}
 
 	/**
