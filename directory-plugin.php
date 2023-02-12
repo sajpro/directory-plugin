9a1b3f0c6b712695bb91163134da4e7dc55a68dc
stalukder03 <stalukder03@gmail.com>
2023-02-12 12:32:59 +0600

    Initial setup with plugin header & phpcs rules

---
+++ directory-plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name:       Directory Plugin
+ * Plugin URI:        http://sajib.me/plugins/directory-plugin/
+ * Description:       A simple and well structured directory plugin where a logged in user will submit listings through a frontend submission form.
+ * Version:           1.0.0
+ * Author:            Sajib Talukder
+ * Author URI:        http://sajib.me/
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       directory-plugin
+ * Domain Path:       /languages
+ *
+ * @package DirectoryPlugin
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
