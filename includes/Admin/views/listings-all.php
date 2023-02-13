9a0feaa783fbcb143f7896110aab51693c7a9cbd
stalukder03 <stalukder03@gmail.com>
2023-02-13 22:44:42 +0600

    display search result for: in listing page

--- includes/Admin/views/listings-all.php
+++ includes/Admin/views/listings-all.php
@@ -2,6 +2,15 @@
 	<h1 class="wp-heading-inline"><?php esc_html_e( 'All Listings', 'directory-plugin' ); ?></h1>
 
 	<a class="page-title-action" href="<?php echo esc_url( admin_url( 'admin.php?page=directory-listings-create' ) ); ?>"><?php esc_html_e( 'Add New', 'directory-plugin' ); ?></a>
+	<?php
+	if ( isset( $_POST['s'] ) && ! empty( $_POST['s'] ) ) {
+		echo sprintf(
+			'<span class="subtitle">%1$s <strong>%2$s</strong></span>',
+			__( 'Search results for:' ),
+			$_POST['s']
+		);
+	}
+	?>
 	<hr class="wp-header-end">
 	
 	<?php
