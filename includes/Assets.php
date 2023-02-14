69ef8a4d2fa79aa37195b12c2b601a4cc9047f89
stalukder03 <stalukder03@gmail.com>
2023-02-14 17:19:49 +0600

    added block styles for frontend & backend both

--- includes/Assets.php
+++ includes/Assets.php
@@ -18,13 +18,18 @@ class Assets {
 		if ( is_admin() ) {
 			$this->blocks_file = include_once DIRECTORY_PLUGIN_PATH . '/build/index.asset.php';
 			add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_assets' ] );
-
 			add_action( 'admin_enqueue_scripts', [ $this, 'admin_assets' ] );
 		}
+
+		add_action( 'enqueue_block_assets', [ $this, 'enqueue_block_assets' ] );
 	}
 
 	public function enqueue_block_editor_assets() {
 		wp_register_script( 'dp-editor-script', DIRECTORY_PLUGIN_BLOCK_ASSETS . '/index.js', $this->blocks_file['dependencies'], $this->blocks_file['version'], true );
+	}
+
+	public function enqueue_block_assets() {
+		wp_enqueue_style( 'dp-block-style', DIRECTORY_PLUGIN_BLOCK_ASSETS . '/blocks-style.css', [], md5_file( DIRECTORY_PLUGIN_BLOCK_ASSETS . '/blocks-style.css' ), 'all' );
 	}
 
 	/**
