51d9ce8d7b3c19428a5a50bef686693113b46da5
stalukder03 <stalukder03@gmail.com>
2023-02-17 10:25:28 +0600

    Inspector Tab added for Content/Style/Advanced

--- assets/blocks/index.asset.php
+++ assets/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '152f9aa43fedd7c6b825');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b8a89cc3ff1700e63715');
--- src/blocks/listings/inspector.js
+++ src/blocks/listings/inspector.js
@@ -3,7 +3,26 @@ import { InspectorControls } from '@wordpress/block-editor';
 import {
 	PanelBody,
 	TextControl,
+    TabPanel
 } from '@wordpress/components';
+
+export const TAB_BUTTONS = [
+    {
+        name: 'general',
+        title: 'General',
+        className: 'fb-tab general',
+    },
+    {
+        name: 'style',
+        title: 'Style',
+        className: 'fb-tab styles',
+    },
+    {
+        name: 'advanced',
+        title: 'Advanced',
+        className: 'fb-tab advanced',
+    },
+];
 
 const Inspector = (props) => {
     let {attributes,setAttributes} = props;
@@ -15,22 +34,38 @@ const Inspector = (props) => {
 
     return (
         <InspectorControls>
-            <PanelBody title={ __( 'Advanced', 'directory-plugin' ) }>
-                <TextControl
-                    label={ __( 'Title', 'directory-plugin' ) }
-                    value={title}
-                    onChange={ (v) =>
-                        setAttributes( { title: v } )
-                    }
-                />
-                <TextControl
-                    label={ __( 'Subitle', 'directory-plugin' ) }
-                    value={subtitle}
-                    onChange={ (v) =>
-                        setAttributes( { subtitle: v } )
-                    }
-                />
-            </PanelBody>
+            <TabPanel
+					className='dp-inspector-tab-panel'
+					activeClass='active-tab'
+					tabs={TAB_BUTTONS}
+            >
+                {(tab)=> <>
+                    {(tab.name == 'general') && (
+                        <PanelBody title={ __( 'Advanced', 'directory-plugin' ) }>
+                            <TextControl
+                                label={ __( 'Title', 'directory-plugin' ) }
+                                value={title}
+                                onChange={ (v) =>
+                                    setAttributes( { title: v } )
+                                }
+                            />
+                            <TextControl
+                                label={ __( 'Subitle', 'directory-plugin' ) }
+                                value={subtitle}
+                                onChange={ (v) =>
+                                    setAttributes( { subtitle: v } )
+                                }
+                            />
+                        </PanelBody>
+                    )}
+                    {(tab.name == 'style') && (
+                        <h2>style</h2>
+                    )}
+                    {(tab.name == 'advanced') && (
+                        <h2>advanced</h2>
+                    )}
+                </>}
+            </TabPanel>
         </InspectorControls>
     )
 }
