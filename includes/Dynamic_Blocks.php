dc3ee7ae13a1a921f60b7c96892dc731ab4d6eca
stalukder03 <stalukder03@gmail.com>
2023-02-18 11:03:27 +0600

    dynamic block class cleanup

--- includes/Dynamic_Blocks.php
+++ includes/Dynamic_Blocks.php
@@ -16,8 +16,11 @@ class Dynamic_Blocks {
 		// Register blocks.
 		add_action( 'init', [ $this, 'register_blocks' ] );
 
+		// Fonts Loader for blocks.
 		new Blocks\FontsLoader();
+		// Register Meta to store selected fonts from blocks.
 		new Blocks\PostMeta();
+		// Generate style file for each page that will be uploaded to upload/dp-style dir.
 		new Blocks\StyleLoader();
 	}
 
@@ -25,7 +28,7 @@ class Dynamic_Blocks {
 	 * Register all dynamic blocks
 	 */
 	public function register_blocks() {
-		// Listings block.
+		// Dynamic Listings block.
 		DynamicBlocks\Listings::register();
 	}
 
