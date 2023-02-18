177393bf3cf3b8141593038331e90ce1c6a194db
stalukder03 <stalukder03@gmail.com>
2023-02-18 18:44:28 +0600

    css unit bug fixed

--- assets/blocks/index.asset.php
+++ assets/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '426a77ec6b67dac3259e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'f43473e1252c1a4d7371');
